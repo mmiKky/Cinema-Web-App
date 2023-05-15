f4c77a88224ea6ae65cf955b9c0ba41eaa59a2e1
mmiKky <mm@DESKTOP-2OTU37S>
2023-05-15 18:52:45 +0200

    initialize Spring Boot app

---
+++ CinemaWebApp/src/main/java/com/example/cinemawebapp/CinemaWebAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.cinemawebapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CinemaWebAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CinemaWebAppApplication.class, args);
+	}
+
+}
---
+++ CinemaWebApp/src/test/java/com/example/cinemawebapp/CinemaWebAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cinemawebapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CinemaWebAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
