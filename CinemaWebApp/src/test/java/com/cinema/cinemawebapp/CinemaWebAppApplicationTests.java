7c9dd51962f8a10e3c6cae9258af4edec90fb9d8
mmiKky <mm@DESKTOP-2OTU37S>
2023-06-14 11:25:30 +0200

    configured docker-compose

--- CinemaWebApp/src/test/java/com/cinema/cinemawebapp/CinemaWebAppApplicationTests.java
+++ CinemaWebApp/src/test/java/com/cinema/cinemawebapp/CinemaWebAppApplicationTests.java
@@ -1,12 +1,12 @@
-package com.cinema.cinemawebapp;
-
-import org.junit.Test;
-
-@SpringBootTest
-class CinemaWebAppApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
-}
+//package com.cinema.cinemawebapp;
+//
+//import org.junit.Test;
+//
+//@SpringBootTest
+//class CinemaWebAppApplicationTests {
+//
+//	@Test
+//	void contextLoads() {
+//	}
+//
+//}
